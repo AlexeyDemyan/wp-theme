75af195eeb9d4b0505a12a5f989978a4a5e56062
Alexey Demyanov <deal110988@him-mail.ch>
2025-04-25 17:21:03 +0200

    Finished Program Post Type

---
+++ single-program.php
@@ -0,0 +1,79 @@
+<?php
+
+get_header();
+while (have_posts()) {
+    the_post(); ?>
+    <div class="page-banner">
+        <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('/images/ocean.jpg') ?>)"></div>
+        <div class="page-banner__content container container--narrow">
+            <h1 class="page-banner__title"><?php the_title() ?></h1>
+            <div class="page-banner__intro">
+                <p>TEXT TO BE REPLACED LATER</p>
+            </div>
+        </div>
+    </div>
+
+    <div class="container container--narrow page-section">
+        <div class="metabox metabox--position-up metabox--with-home-link">
+            <p>
+                <a class="metabox__blog-home-link" href="<?php echo get_post_type_archive_link('program') ?>"><i class="fa fa-home" aria-hidden="true"></i> All Programs</a> <span class="metabox__main"><?php the_title() ?></span>
+            </p>
+        </div>
+        <div class="generic-content">
+            <?php the_content() ?></div>
+        <?php
+        $today = date('Ymd');
+        $eventPosts = new WP_Query(array(
+            // posts_pet_page set at -1 will give all posts that meet the conditions
+            'posts_per_page' => 2,
+            'post_type' => 'event',
+            'meta_key' => 'event_date',
+            'orderby' => 'meta_value_num',
+            'order' => 'ASC',
+            'meta_query' => array(
+                array(
+                    'key' => 'event_date',
+                    'compare' => '>=',
+                    'value' => $today,
+                    'type' => 'numeric'
+                ),
+                // here we're filtering for events that have the current program as their related program
+                array(
+                    'key' => 'related_program',
+                    'compare' => 'LIKE',
+                    'value' => '"' . get_the_ID() . '"' // IMP! We're sanitizing the value with quotes to account for issue with serialization
+                )
+            )
+        ));
+
+        if ($eventPosts->have_posts()) {
+            echo '<hr class="section-break">';
+            echo '<h2 class="headline headline--medium">Upcoming ' . get_the_title() . ' Events</h2>';
+            echo '<br>';
+            while ($eventPosts->have_posts()) {
+                $eventPosts->the_post(); ?>
+                <div class="event-summary">
+                    <a class="event-summary__date t-center" href="#">
+                        <span class="event-summary__month"><?php
+                                                            $eventDate = new DateTime(get_field('event_date'));
+                                                            echo $eventDate->format('M'); ?></span>
+                        <span class="event-summary__day"><?php echo $eventDate->format('d'); ?></span>
+                    </a>
+                    <div class="event-summary__content">
+                        <h5 class="event-summary__title headline headline--tiny"><a href="<?php the_permalink() ?>"><?php the_title() ?></a></h5>
+                        <p><?php if (has_excerpt()) {
+                                // here we echo the getter instead of normal the_excerpt for better styling
+                                echo get_the_excerpt();
+                            } else {
+                                echo wp_trim_words(get_the_content(), 18);
+                            } ?><a href="<?php the_permalink() ?>" class="nu gray"> Learn more</a></p>
+                    </div>
+                </div>
+        <?php }
+        }
+        ?>
+    </div>
+<?php
+};
+
+get_footer();

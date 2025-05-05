7394b84ca934d68cfccd7a39d4e3ab05ad5677f8
Alexey Demyanov <deal110988@him-mail.ch>
2025-05-05 16:48:16 +0200

    working on Live Search functionality

--- footer.php
+++ footer.php
@@ -21,7 +21,7 @@
                 <div class="site-footer__col-three">
                     <h3 class="headline headline--small">Learn</h3>
                     <nav class="nav-list">
-                    <?php wp_nav_menu((array(
+                        <?php wp_nav_menu((array(
                             'theme_location' => 'footerLocationTwo'
                         ))) ?>
                     </nav>
@@ -54,6 +54,16 @@
         </div>
     </div>
 </footer>
+
+<div class="search-overlay">
+    <div class="search-overlay__top">
+        <div class="container">
+            <i class="fa fa-search search-overlay__icon" aria-hidden="true"></i>
+            <input id="search-term" type="text" class="search-term" placeholder="What are you looking for?">
+            <i class="fa fa-window-close search-overlay__close" aria-hidden="true"></i>
+        </div>
+    </div>
+</div>
 
 <?php wp_footer(); ?>
 </body>

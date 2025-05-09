9f6ebc19d715e3c07daa4b682657ca2f0eb02a4c
Alexey Demyanov <deal110988@him-mail.ch>
2025-05-09 16:01:02 +0200

    pretty much finished Search functionality

--- includes/search-route.php
+++ includes/search-route.php
@@ -41,6 +41,16 @@ function universitySearchResults($data)
         }
 
         if (get_post_type() == 'program') {
+            $relatedCampuses = get_field('related_campus');
+
+            if ($relatedCampuses) {
+                foreach ($relatedCampuses as $item) {
+                    array_push($results['campuses'], array(
+                        'title' => get_the_title($item),
+                        'permalink' => get_the_permalink($item)
+                    ));
+                }
+            }
 
             array_push($results['programs'], array(
                 'title' => get_the_title(),
@@ -76,53 +86,71 @@ function universitySearchResults($data)
         }
     }
 
-    $programsMetaQuery = array(
-        'relation' => 'OR'
-    );
+    // This IF check is so that if $programsMetaQuery returns empty, we return no professors
+    if ($results['programs']) {
+        $programsMetaQuery = array(
+            'relation' => 'OR'
+        );
 
-    foreach ($results['programs'] as $item) {
-        array_push($programsMetaQuery, array(
-            'key' => 'related_program',
-            'compare' => 'LIKE',
-            // We're surrounding result with quotes here:
-            'value' => '"' . $item['id'] . '"'
-        ));
-    };
-
-    $programRelationshipQuery = new WP_Query(array(
-        'post_type' => 'professor',
-        'meta_query' => $programsMetaQuery
-        // This was the first draft, will leave it in the commit:
-        
-        // 'meta_query' => array(
-        //     array(
-        //         'key' => 'related_program',
-        //         'compare' => 'LIKE',
-        //         // We're surrounding result with quotes here:
-        //         'value' => '"' . $results['programs'][0]['id'] . '"'
-        //     )
-        // )
-    ));
-
-    while ($programRelationshipQuery->have_posts()) {
-        $programRelationshipQuery->the_post();
-
-        if (get_post_type() == 'professor') {
-            array_push($results['professors'], array(
-                'title' => get_the_title(),
-                'permalink' => get_the_permalink(),
-                // 0 here refers to current post
-                'image' => get_the_post_thumbnail_url(0, 'professorLandscape')
+        foreach ($results['programs'] as $item) {
+            array_push($programsMetaQuery, array(
+                'key' => 'related_program',
+                'compare' => 'LIKE',
+                // We're surrounding result with quotes here:
+                'value' => '"' . $item['id'] . '"'
             ));
+        };
+
+        $programRelationshipQuery = new WP_Query(array(
+            'post_type' => array('professor', 'event'),
+            'meta_query' => $programsMetaQuery
+            // This was the first draft, will leave it in the commit:
+
+            // 'meta_query' => array(
+            //     array(
+            //         'key' => 'related_program',
+            //         'compare' => 'LIKE',
+            //         // We're surrounding result with quotes here:
+            //         'value' => '"' . $results['programs'][0]['id'] . '"'
+            //     )
+            // )
+        ));
+
+        while ($programRelationshipQuery->have_posts()) {
+            $programRelationshipQuery->the_post();
+
+            if (get_post_type() == 'event') {
+                $eventDate = new DateTime(get_field('event_date'));
+                $description = null;
+                if (has_excerpt()) {
+                    $description = get_the_excerpt();
+                } else {
+                    $description = wp_trim_words(get_the_content(), 18);
+                }
+
+                array_push($results['events'], array(
+                    'title' => get_the_title(),
+                    'permalink' => get_the_permalink(),
+                    'month' => $eventDate->format('M'),
+                    'day' => $eventDate->format('d'),
+                    'description' => $description
+                ));
+            }
+
+            if (get_post_type() == 'professor') {
+                array_push($results['professors'], array(
+                    'title' => get_the_title(),
+                    'permalink' => get_the_permalink(),
+                    // 0 here refers to current post
+                    'image' => get_the_post_thumbnail_url(0, 'professorLandscape')
+                ));
+            }
         }
+
+        // This is a PHP function that removes duplicates from the search
+        $results['professors'] = array_unique($results['professors'], SORT_REGULAR);
+        $results['events'] = array_unique($results['events'], SORT_REGULAR);
     }
-
-    // This is a PHP function that's supposed to remove duplicates
-    // However, I did not experience duplicates in my search
-    // Although I specifically added some duplicates in WP-Admin
-    // Maybe at this point WP takes care of it out-of-the-box
-    // $results['professors'] = array_unique($results['professors'], SORT_REGULAR);
-
     return $results;
 };
 
